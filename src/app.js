535d3d6296fe722449ce4f56e33685c0ff43507a
Paulo-galvao <paulogalvaoj21@gmail.com>
2024-10-07 20:46:54 -0300

    okay

--- src/app.js
+++ src/app.js
@@ -10,11 +10,12 @@ const port = process.env.PORT;
 
 app.use(e.json());
 
+app.get('/', (req, res) => res.json("okay"));
 app.use('/user', userRouter);
 app.use('/movie', movieRouter);
 app.use('/rent', rentRouter);
 app.use('/recommend', recommendRouter);
 
 app.listen(port, function() {
-    console.log("Server running at port", port);
+    console.log("Server running at port", port); 
 });
